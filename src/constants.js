ff6b7136719c324e2ddf1b930dbc927da238f14b
mhasantech <mhasantech096@gmail.com>
2026-08-01 23:17:46 +0600

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,6 @@ const BASE_URL = "https://dsebd.org";
 export default {
   LATEST_STOCK: `${BASE_URL}/latest_share_price_scroll_l.php`,
   DSEX_DATA: `${BASE_URL}/dseX_share.php`,
-  HISTORY_DATA: `${BASE_URL}/day_end_archive`,
+  HISTORY_DATA: `${BASE_URL}/day_end_archive.php`, // 🔥 এই লাইনে .php যুক্ত করা হয়েছে
+  TOP_30: `${BASE_URL}/latest_share_price_scroll_l.php`,
 };
